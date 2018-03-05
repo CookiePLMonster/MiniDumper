f84d786dff8767ff677521e52c7d2a44edece19f
Silent <zdanio95@gmail.com>
2018-03-05 01:00:06 +0100

    Formatting fixes (include exe filename in dump name)
    Made messagebox optional

--- MiniDumper/dllmain.cpp
+++ MiniDumper/dllmain.cpp
@@ -4,24 +4,43 @@
 #include <time.h>
 #include <DbgHelp.h>
 
+#define INCLUDE_MESSAGEBOX 0
+
 LONG WINAPI CustomUnhandledExceptionFilter(LPEXCEPTION_POINTERS ExceptionInfo)
 {
         // step 1: write minidump
-        char		error[1024];
-		char		errorCode[1024];
-        char		filename[MAX_PATH];
+#if INCLUDE_MESSAGEBOX
+        wchar_t		error[1024];
+		wchar_t		errorCode[1024];
+#endif
+		wchar_t		modulename[MAX_PATH];
+		wchar_t		filename[MAX_PATH];
+		wchar_t		timestamp[128];
         __time64_t	time;
         struct tm	ltime;
 		HANDLE		hFile;
 		HWND		hWnd;
 
+		const wchar_t* modulenameptr;
+		if ( GetModuleFileNameW( GetModuleHandle(NULL), modulename, _countof(modulename)) != 0 )
+		{
+			modulenameptr = wcsrchr(modulename, '\\') + 1;
+		}
+		else
+		{
+			modulenameptr = L"err.err";
+		}
+
         _time64(&time);
         _localtime64_s(&ltime, &time);
-        strftime(filename, sizeof(filename) - 1, "minidump-%Y%m%d%H%M%S.dmp", &ltime);
-        sprintf_s(error, sizeof(error), "A minidump has been written to %s.", filename);
+        wcsftime(timestamp, _countof(timestamp), L"%Y%m%d%H%M%S", &ltime);
+		swprintf_s(filename, L"%s.%s.dmp", modulenameptr, timestamp);
+#if INCLUDE_MESSAGEBOX
+		swprintf_s(error, L"A minidump has been written to %s.", filename);
+#endif
 
         //CreateDirectoryA("minidumps", NULL);
-		hFile = CreateFileA(filename, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+		hFile = CreateFileW(filename, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
         if (hFile != INVALID_HANDLE_VALUE)
         {
@@ -31,26 +50,37 @@ LONG WINAPI CustomUnhandledExceptionFilter(LPEXCEPTION_POINTERS ExceptionInfo)
                 ex.ExceptionPointers = ExceptionInfo;
                 ex.ClientPointers = TRUE;
 
-                if (FAILED(MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hFile, MiniDumpNormal, &ex, NULL, NULL)))
-                        sprintf_s(error, sizeof(error), "An error (0x%X) occurred during writing %s.", GetLastError(), filename);
+                if (FAILED(MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hFile, MiniDumpWithDataSegs, &ex, NULL, NULL)))
+				{
+#if INCLUDE_MESSAGEBOX
+					swprintf_s(error, L"An error (0x%X) occurred during writing %s.", GetLastError(), filename);
+#endif
+				}
 
                 CloseHandle(hFile);
         }
         else
-			sprintf_s(error, sizeof(error), "An error (0x%X) occurred during creating %s.", GetLastError(), filename);
+		{
+#if INCLUDE_MESSAGEBOX
+			swprintf_s(error, L"An error (0x%X) occurred during creating %s.", GetLastError(), filename);
+#endif
+		}
 
         // step 2: exit the application
-		sprintf_s(errorCode, sizeof(errorCode), "Fatal error (0x%08X) at 0x%08X.\n%s", ExceptionInfo->ExceptionRecord->ExceptionCode, ExceptionInfo->ExceptionRecord->ExceptionAddress, error);
+#if INCLUDE_MESSAGEBOX
+		swprintf_s(errorCode, L"Fatal error (0x%08X) at 0x%08X.\n%s", ExceptionInfo->ExceptionRecord->ExceptionCode, ExceptionInfo->ExceptionRecord->ExceptionAddress, error);
+#endif
 		ShowCursor(TRUE);
-		hWnd = FindWindowA(0, "");
+		hWnd = FindWindowW(0, L"");
 		SetForegroundWindow(hWnd);
+#if INCLUDE_MESSAGEBOX
 		ShowCursor(TRUE);
-		MessageBoxA(NULL, errorCode, "MiniDumper", MB_ICONERROR | MB_OK );
-		hWnd = FindWindowA(0, "");
+		MessageBoxW(NULL, errorCode, L"MiniDumper", MB_ICONERROR | MB_OK );
+		hWnd = FindWindowW(0, L"");
 		SetForegroundWindow(hWnd);
+#endif
 
-		ExitProcess(0);
-       // return 0;
+		return EXCEPTION_CONTINUE_SEARCH;
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
